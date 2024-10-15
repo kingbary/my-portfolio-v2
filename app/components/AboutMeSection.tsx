91bba1d0dd992f9fc092639d32f19e25c1dd8141
Kingsley <kingsleyakwa@gmail.com>
2024-10-15 20:32:52 +0100

    Feat: add content to main section

---
+++ app/components/AboutMeSection.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function AboutMeSection() {
+    return (
+        <section className="mt-10">
+            <p className="text-sm text-darkGray leading-5 md:text-xl dark:text-lightGray">
+                Hello! I&apos;m Kingsley Akwa, a front-end engineer crafting exceptional web experiences.
+                I specialize in NextJS and React for cutting-edge web development. I have hands-on experience
+                in projects involving React, Redux, and Typescript, contributing to developing web-based chat
+                platforms and admin panels.
+            </p>
+            <br />
+            <p className="text-sm text-darkGray leading-5 md:text-xl dark:text-lightGray">
+                I have expertise in languages like JavaScript, Typescript, Python and PHP specializing in ReactJS,
+                NextJS, NodeJS, and Mantine UI within web development. I love to mess around with complicated
+                Typescript in my free time.
+            </p>
+            <br />
+            <p className="text-sm text-darkGray leading-5 md:text-xl dark:text-lightGray">
+                As a tech enthusiast, I&apos;m also constantly updated with the newest innovations
+                (mobile phones, computers, gadgets, you name it) and most recently, waiting eagerly to try out
+                the new AI features in iOS 18.
+            </p>
+        </section>
+    )
+}
+
+export default AboutMeSection
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import AboutMeSection from "./components/AboutMeSection";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-3xl font-bold text-darkGray dark:text-customGray">
-        Kingsley Akwa
-      </p>
-    </main>
+    <div className="flex min-h-screen w-full max-w-[624px] flex-col pt-20 px-4">
+      <div className="flex w-full justify-between items-center">
+        <div className="size-[96px] rounded-xl bg-[url(/images/headshot.jpg)] bg-center bg-cover shadow"></div>
+        <div>
+          <h2 className="text-2xl text-black-500 dark:text-white font-bold md:text-[40px]">Kingsley Akwa</h2>
+          <p className="text-sm text-darkGray leading-5 mt-1 dark:text-lightGray md:text-xl">Front-end Engineer</p>
+        </div>
+      </div>
+      <AboutMeSection />
+    </div>
   );
 }
