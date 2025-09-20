3b2cb42a5aafdf908a528ad6cc89dc53b9b29158
kingbary <kingsleyakwa@gmail.com>
2025-09-20 07:40:00 +0100

    feat: google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,17 @@ export default function RootLayout({
             gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KBJVNHNH');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className={`${bricolageGrotesque.className} relative h-screen`}>
         {children}
